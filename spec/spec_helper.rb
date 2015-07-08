c19758971d9ba9615d7c9df0bc225f872dffd33b
Eddie Johnston <eddie@beanstalk.ie>
2015-07-08 11:42:19 +0100

    Integrated Travis and Covereall

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,11 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
+
+require 'coveralls'
+Coveralls.wear!
+
 require 'seadragon'
+
+RSpec.configure do |config|
+  config.filter_run :focus
+  config.run_all_when_everything_filtered = true
+end
